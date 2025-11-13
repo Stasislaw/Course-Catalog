0200d55416819abc10b96dcd403ea9ebf1b75c8c
Staluk <staluk08@gmail.com>
2025-11-13 18:12:38 +0100

    Added searching (kinda)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,86 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { he } from "zod/locales";
+import { useForm } from "react-hook-form";
+
+function useDebounce(value, delay) {
+  const [debounced, setDebounced] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => setDebounced(value), delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debounced;
+}
 
 export default function Home() {
 
   const [categories, setCategories] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [courses, setCourses] = useState([]);
+
+  const { register, watch } = useForm({
+    defaultValues: {
+      search: ""
+    }
+  });
+  const search = watch("search");
+  const debouncedSearch = useDebounce(search, 300);
+
 
   useEffect(() => {
     fetch("http://localhost:3001/categories")
       .then(res => res.json())
       .then(data => {
         setCategories(data);
-        setLoading(false);
-      })
-  }, []);
-
-  return (
+      });
+    }, []);
+  useEffect(() => {
+    if(debouncedSearch){
+      const encodedSearch = encodeURIComponent(debouncedSearch);
+      fetch(`http://localhost:3001/courses?q=${encodedSearch}&_page=1&_limit=12`)
+      .then(res => res.json())
+      .then(data => {
+        setCourses(data);
+      });
+    }else{
+      setCourses([]);
+    }
+  }, [debouncedSearch]);
+    
+    return (
     <>
       <h1>Home</h1>
-      {/* //pasek wyszukiwania */}
+      {/* Search bar */}
+      <form>
+        <input type="text" placeholder="Search courses" {...register("search")}/>
+      </form>
+      {/* Search results */}
+      <div>
+        {courses.length === 0 ? (
+          "Nothing to search"
+        ) : (
+          <ul>
+            {courses.map(course => (
+              <li key={course.id}>
+                <Link to={`courseDetails?courseId=${course.id}`}>
+                  <img src={course.thumbnail} alt={course.slug}
+                  style={{
+                    width: 20,
+                    height: 20
+                  }}
+                  />
+                  <h3>{course.title}</h3>
+                  {course.shortDesc}<br/>
+                  Rating: {course.rating}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      
+      {/* Categories */}
       <div>
         <h2>Reccomended categories</h2>
         <ul>
